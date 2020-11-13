688fa715bc9aed97ab9a737cec32c0a7e2d99724
Kevin Gessner <kevin@kevingessner.com>
2020-11-13 17:41:24 -0500

    arduino position value updates from dad

--- ArduinoCodes/arduino3servoTest/arduino3servoTest.ino
+++ ArduinoCodes/arduino3servoTest/arduino3servoTest.ino
@@ -1,38 +1,47 @@
+//arduino3servoTestEdits
 #include <Servo.h>
-
 Servo servoA;
 Servo servoB;
 Servo servoC;
 
-int ledTemoin = 8;
-float angleA = 5;
-float angleB = 5;
-float angleC = 3;
+int ledTemoin = 13;  // changed from 8 so will use onboard LED
+float angleA = 25;
+float angleB = 25;
+float angleC = 25;
+//float angleA = 5;
+//float angleB = 5;
+//float angleC = 3;
 
 void setup() {
   Serial.begin(19200);
-  pinMode(ledTemoin, OUTPUT); // led temoin
+  pinMode(ledTemoin, OUTPUT); // led indicator [ledTemoin]
   digitalWrite(ledTemoin,HIGH);
   servoA.attach(9);     //servo A
   servoB.attach(11);    //servo B
   servoC.attach(10);    //servo C
   delay(1000);
-  servoA.writeMicroseconds((-2160+1260)*float(angleA)/90 + 2160);
-  servoB.writeMicroseconds((-1975+1130)*float(angleB)/90 + 1975);
-  servoC.writeMicroseconds((-1955+1130)*float(angleC)/90 + 1955);
-}
+    // playing with numbers
+  
+  servoA.writeMicroseconds((-550+900)*float(angleA)/90 + 550);
+  servoB.writeMicroseconds((-550+900)*float(angleB)/90 + 550);
+  servoC.writeMicroseconds((-550+900)*float(angleC)/90 + 550);
+  //servoA.writeMicroseconds((-2160+1260)*float(angleA)/90 + 2160);  //original numbers
+  //servoB.writeMicroseconds((-1975+1130)*float(angleB)/90 + 1975);
+  //servoC.writeMicroseconds((-1955+1130)*float(angleC)/90 + 1955);
+  }
 
 int count = 0;
 
 void loop() {
-  digitalWrite(ledTemoin , millis() / 500 % 2 ); // led temoin clignotement
+  digitalWrite(ledTemoin , millis() / 100 % 2 ); // led indicator blinking [temoin clignotement]
   
   if(Serial.available() > 0) {
     String a = Serial.readStringUntil('\n');
-    if(a == "descendreBras"){
-      angleA = 5;
-      angleB = 5;
-      angleC = 3;
+    if(a == "descendreBras"){     // arms fully down
+      angleA = 25;
+      angleB = 25;
+      angleC = 25;
+      //angleC = 3;
     }else{
       a.remove(0,1);
       a.remove(a.length() - 1,1); 
@@ -40,9 +49,12 @@ void loop() {
       angleB = getValue(a, ',', 1).toFloat();    
       angleC = getValue(a, ',', 2).toFloat();  
     }
-    servoA.writeMicroseconds((-2160+1260)*float(angleA)/90 + 2160);
-    servoB.writeMicroseconds((-1975+1130)*float(angleB)/90 + 1975);
-    servoC.writeMicroseconds((-1955+1130)*float(angleC)/90 + 1955);
+    servoA.writeMicroseconds((-550+900)*float(angleA)/90 + 550); 
+    servoB.writeMicroseconds((-550+900)*float(angleB)/90 + 550);
+    servoC.writeMicroseconds((-550+900)*float(angleC)/90 + 550);
+    //servoA.writeMicroseconds((-2160+1260)*float(angleA)/90 + 2160);
+   //servoB.writeMicroseconds((-1975+1130)*float(angleB)/90 + 1975);
+    //servoC.writeMicroseconds((-1955+1130)*float(angleC)/90 + 1955);
   }
 
 }
